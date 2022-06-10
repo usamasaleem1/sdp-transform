0277bbe4b63828e6e464ceb28b2b061574f5aa60
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2022-06-10 14:08:40 -0400

    ss

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<?php include_once('home.html');
+<php>
+    <!DOCTYPE HTML>
+    <html>
+
+    <head>
+        <link rel="stylesheet" href="style.css">
+
+
+    </head>
+
+    <!-- Include Font awesome cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+
+    <body>
+        <!-- include javascript script.js-->
+
+        <div class="drop-container">
+            <div class="drop">
+                <i class="fa-solid fa-photo-film icon"></i>
+                <span class="text">
+                Drag and drop your documents, photos, and video here.
+            </span>
+                <div class="or-con">
+                    <span class="line"></span>
+                    <span class="or">OR</span>
+                    <span class="line"></span>
+                </div>
+                <label for="file-upload">Browse Files</label>
+                <input type="file" id="file-upload" class="file-input" multiple />
+            </div>
+            <div class="progress"></div>
+        </div>
+        <script src="scriptq.js"></script>
+    </body>
+
+    </html>
+</php>
